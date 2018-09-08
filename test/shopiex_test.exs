0c80dc04670c5b30b7728c0ce294e274b817730c
Jordan Parker <letsgetjordan@gmail.com>
2018-09-08 22:45:47 +0100

    Calling get/post with list runs a loop

--- test/shopiex_test.exs
+++ test/shopiex_test.exs
@@ -26,6 +26,13 @@ defmodule ShopiexTest do
     assert env.body == "ok"
   end
 
+  test "Get is_list" do
+    request = Shopiex.client("valid_store", "token")
+    |> Shopiex.Meta.shop_details()
+
+    assert [%Tesla.Env{}, %Tesla.Env{}] = Shopiex.go(:get, [request, request])
+  end
+
   test "Post" do
     request = Shopiex.client("valid_store", "token")
     |> Shopiex.Webhooks.create(verb: "verb", filter: "filter")
@@ -33,5 +40,12 @@ defmodule ShopiexTest do
     assert {:ok, %Tesla.Env{} = env} = Shopiex.go(:post, request)
     assert env.status == 200
     assert env.body == "ok"
+  end
+
+  test "Post is_list" do
+    request = Shopiex.client("valid_store", "token")
+    |> Shopiex.Webhooks.create(verb: "verb", filter: "filter")
+
+    assert [%Tesla.Env{}, %Tesla.Env{}] = Shopiex.go(:post, [request, request])
   end
 end
